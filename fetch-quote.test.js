d5f2b2ed059404afc9ef1cfcc08da6fc777facc1
Richard Hillman <Richard.CC.Hillman@gmail.com>
2020-12-01 17:28:10 -0800

    regular

--- fetch-quote.test.js
+++ fetch-quote.test.js
@@ -11,9 +11,9 @@ describe('fetchQuotes', () => {
         const fetchedQuote = await fetchQuote();
         
         expect(fetchedQuote).toEqual({
-            character: expect.any(String),
-            quote: expect.any(String),
-            image: expect.any(String)
+            character: "URL",
+            quote: "You call that an antenna?",
+            image: "https://res.cloudinary.com/dzxqhkyqd/image/upload/v1554904048/Url.jpg"
         })
     })
 
